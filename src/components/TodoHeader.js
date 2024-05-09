1821235a0e0194376570c8ab9d7f8776acb231b5
alsgurdl <fdf98952@gmail.com>
2024-05-09 12:34:50 +0900

    5/9

---
+++ src/components/TodoHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../scss/TodoHeader.scss';
+const TodoHeader = () => {
+  return (
+    <header>
+      <h1>2024년 5월 9일</h1>
+      <div className="day">목요일</div>
+      <div className="tasks-left">한 일 x개 남음</div>
+    </header>
+  );
+};
+
+export default TodoHeader;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TodoItem = () => {
+  return <li className="todo-list-item"></li>;
+};
+
+export default TodoItem;
