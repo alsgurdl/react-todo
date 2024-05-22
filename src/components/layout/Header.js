93222d9c8e387fb0779330f185748af062eca7b4
alsgurdl <fdf98952@gmail.com>
2024-05-22 16:03:02 +0900

    5/21

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,12 +4,25 @@ import {
   Toolbar,
   Typography,
 } from '@mui/material';
-import React, { useContext, useEffect } from 'react';
+import React, {
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import '../../scss/Header.scss';
 import AuthContext from '../../utils/AuthContext';
+import {
+  API_BASE_URL,
+  USER,
+} from '../../config/host-config';
 
 const Header = () => {
+  const profileRequestURL = `${API_BASE_URL}${USER}/load-profile`;
+
+  //프로필 이미지 url 상태
+  const [profileUrl, setprofileUrl] = useState(null);
+
   //authconte
   const redirection = useNavigate();
   const { isLoggedIn, userName, onLogout } =
@@ -21,6 +34,34 @@ const Header = () => {
     redirection('/login');
   };
 
+  //프로필 이미지 요청
+  const fetchProfileImage = async () => {
+    const res = await fetch(profileRequestURL, {
+      method: 'GET',
+      headers: {
+        Authorization:
+          'Bearer ' + localStorage.getItem('ACCESS_TOKEN'),
+      },
+    });
+    if (res.status === 200) {
+      //서버에서는 바이트로 직렬화된 이미지가 응답되므로
+      //blob()을 통해 전달받아야 한다
+      const profileBlob = await res.blob();
+      //이미지를 imgurl로 변경
+      const imgUrl =
+        window.URL.createObjectURL(profileBlob);
+      setprofileUrl(imgUrl);
+    } else {
+      const err = await res.text();
+      console.log('err: ', err);
+      setprofileUrl(null);
+    }
+  };
+  //로그인 상태가 변화할 때 화면이 리렌더링 되고
+  //그에 맞는 회원의 프로필 이미지 요청이 들어갈 수
+  useEffect(() => {
+    if (isLoggedIn) fetchProfileImage();
+  }, [isLoggedIn]);
   return (
     <AppBar
       position="fixed"
@@ -42,6 +83,21 @@ const Header = () => {
                 {isLoggedIn ? userName + '님' : '오늘'}의
                 할일
               </Typography>
+              {isLoggedIn && (
+                <img
+                  src={
+                    profileUrl ||
+                    require('../../assets/img/anonymous.jpg')
+                  }
+                  alt="프사"
+                  style={{
+                    marginLeft: 20,
+                    width: 75,
+                    height: 75,
+                    borderRadius: '50%',
+                  }}
+                />
+              )}
             </div>
           </Grid>
 
